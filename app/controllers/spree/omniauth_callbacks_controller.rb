277e7fef139add3d097003d24c1280c12ec9cd33
DavideMiozzi <davide.miozzi@gmail.com>
2013-08-31 09:59:35 +0200

    Restored redirect

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -17,9 +17,11 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
 
           if authentication.present?
-            flash[:notice] = "Signed in successfully 111"
-            sign_in :spree_user, authentication.user
-            redirect_to checkout_state_path(current_order.checkout_steps.first) 
+            sign_in_and_redirect :spree_user, authentication.user
+            if current_order
+              redirect_to checkout_state_path(current_order.checkout_steps.first)
+            end
+             
           elsif spree_current_user
             spree_current_user.apply_omniauth(auth_hash)
             spree_current_user.save!
@@ -29,7 +31,6 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
             user.apply_omniauth(auth_hash)
             if user.save
-              flash[:notice] = "Signed in successfully. 222"
               sign_in_and_redirect :spree_user, user
             else
               session[:omniauth] = auth_hash.except('extra')
